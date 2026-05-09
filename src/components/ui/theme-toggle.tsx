0bc56604108fcb9be4f0967fc7e9e19d1bb8c9a4
suyogvedak <suyogvedak2@gmail.com>
2026-05-09 20:47:47 +0530

    new file:   src/components/ui/theme-toggle.tsx

---
+++ src/components/ui/theme-toggle.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <button
+      onClick={() =>
+        setTheme(theme === "dark" ? "light" : "dark")
+      }
+      className="
+        w-10 h-10
+        rounded-full
+        flex items-center justify-center
+        border border-[var(--border)]
+        bg-[var(--card)]
+        backdrop-blur-md
+        hover:scale-105
+        transition
+      "
+    >
+      {theme === "dark" ? (
+        <Sun size={18} />
+      ) : (
+        <Moon size={18} />
+      )}
+    </button>
+  );
+}
