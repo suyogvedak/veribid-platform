5f167bb47f2d867834595baa0549e03c7ca14f57
suyogvedak <suyogvedak2@gmail.com>
2026-05-09 20:26:56 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Navbar from "@/components/navbar/navbar";
+
 export default function HomePage() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        VeriBid Marketplace
-      </h1>
+    <main className="min-h-screen">
+      <Navbar />
+
+      <section className="h-screen flex items-center justify-center">
+        <h1 className="text-7xl font-bold text-center">
+          VeriBid Marketplace
+        </h1>
+      </section>
     </main>
   );
 }
