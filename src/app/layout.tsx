92c5e515843644ddace43746ff5f7a373edc6207
suyogvedak <suyogvedak2@gmail.com>
2026-05-09 20:43:28 +0530

    inital changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 export const metadata = {
   title: "VeriBid",
@@ -7,12 +8,16 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
