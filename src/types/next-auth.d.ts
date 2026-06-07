d75401dd9ac7ead68e31270e7fa06ccd43009376
suyogvedak <suyogvedak2@gmail.com>
2026-06-07 19:30:23 +0530

    refactor: simplify TypeScript definitions for NextAuth session and JWT

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,23 +1,19 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-
-      role: string;
-
-      isVerified: boolean;
-    } & DefaultSession["user"];
-  }
+interface Session {
+user: {
+id: string;
+role: string;
+isVerified: boolean;
+} & DefaultSession["user"];
+}
 }
 
 declare module "next-auth/jwt" {
-  interface JWT {
-    id: string;
-
-    role: string;
-
-    isVerified: boolean;
-  }
+interface JWT {
+id: string;
+role: string;
+isVerified: boolean;
+}
 }
