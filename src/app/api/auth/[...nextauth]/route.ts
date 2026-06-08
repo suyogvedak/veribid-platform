003dc78140abb59f4b3384325bb8944f0f2cb577
suyogvedak <suyogvedak2@gmail.com>
2026-06-08 21:54:38 +0530

    refactor: update export syntax for NextAuth handler

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
-
 import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handler;
+export const POST = handler;
