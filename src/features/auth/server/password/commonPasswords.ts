112276564361d6ff1014226005b75e2bb134119d
suyogvedak <suyogvedak2@gmail.com>
2026-07-04 18:21:28 +0530

    feat: add common passwords list for password validation

---
+++ src/features/auth/server/password/commonPasswords.ts
@@ -0,0 +1,47 @@
+/**
+ * Top Common Passwords
+ *
+ * Used for password validation.
+ * This list intentionally contains only
+ * the most common passwords.
+ *
+ * In production we can later replace this
+ * with a much larger dataset or HIBP API.
+ */
+
+export const COMMON_PASSWORDS = new Set([
+  "123456",
+  "123456789",
+  "12345678",
+  "password",
+  "password123",
+  "admin",
+  "admin123",
+  "qwerty",
+  "qwerty123",
+  "welcome",
+  "welcome123",
+  "abc123",
+  "letmein",
+  "111111",
+  "000000",
+  "football",
+  "monkey",
+  "dragon",
+  "master",
+  "login",
+  "root",
+  "administrator",
+  "passw0rd",
+  "password1",
+  "iloveyou",
+  "sunshine",
+  "princess",
+  "superman",
+  "trustno1",
+  "hello123",
+  "india123",
+  "qazwsx",
+  "asdfgh",
+  "zxcvbn",
+]);
