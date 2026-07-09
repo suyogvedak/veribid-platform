7ac2a3b7af4e035ee186cdf49b44ce0f39a5a4cb
suyogvedak <suyogvedak2@gmail.com>
2026-07-09 22:14:24 +0530

    feat: add Account Linking constants for provider management

---
+++ src/features/auth/server/accountLinking/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * Account Linking Constants
+ */
+
+export const ACCOUNT_LINKING = {
+
+  MAX_LINKED_PROVIDERS: 5,
+
+  GOOGLE_PROVIDER: "google",
+
+  CREDENTIALS_PROVIDER: "credentials",
+
+} as const;
